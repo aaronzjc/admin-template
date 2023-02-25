38c3da38c56d46f28eaed36285c95bc72d70432c
aaronzjc <415397228@qq.com>
2023-02-25 15:29:15 +0800

    userinfo

--- src/components/menu/Menu.vue
+++ src/components/menu/Menu.vue
@@ -43,50 +43,50 @@
                     <a class="navbar-link is-arrowless">
                         <div class="is-user-avatar">
                             <img
-                                src="https://avatars.dicebear.com/v2/initials/john-doe.svg"
-                                alt="John Doe"
+                                :src="mainStore.userInfo.avatar"
+                                :alt="mainStore.userInfo.username"
                             />
                         </div>
                         <div class="is-user-name">
-                            <span>John Doe</span>
+                            <span>{{ mainStore.userInfo.username }}</span>
                         </div>
                         <span class="icon"
                             ><i class="mdi mdi-chevron-down"></i
                         ></span>
                     </a>
                     <div class="navbar-dropdown">
-                        <a href="profile.html" class="navbar-item">
+                        <a href="#" class="navbar-item">
                             <span class="icon"
                                 ><i class="mdi mdi-account"></i
                             ></span>
-                            <span>My Profile</span>
+                            <span>个人资料</span>
                         </a>
                         <a class="navbar-item">
                             <span class="icon"
                                 ><i class="mdi mdi-settings"></i
                             ></span>
-                            <span>Settings</span>
+                            <span>系统设置</span>
                         </a>
                         <hr class="navbar-divider" />
                         <a class="navbar-item">
                             <span class="icon"
                                 ><i class="mdi mdi-logout"></i
                             ></span>
-                            <span>Log Out</span>
+                            <span>退出登录</span>
                         </a>
                     </div>
                 </div>
                 <a
-                    href="https://justboil.me/bulma-admin-template/free-html-dashboard/"
-                    title="About"
+                    href="https://github.com/aaronzjc"
+                    title="关于"
                     class="navbar-item has-divider is-desktop-icon-only"
                 >
                     <Icon :path="mdiHelpCircleOutline"></Icon>
-                    <span>About</span>
+                    <span>关于</span>
                 </a>
-                <a title="Log out" class="navbar-item is-desktop-icon-only">
+                <a title="退出登录" class="navbar-item is-desktop-icon-only">
                     <Icon :path="mdiLogout"></Icon>
-                    <span>Log out</span>
+                    <span>退出登录</span>
                 </a>
             </div>
         </div>
@@ -119,6 +119,7 @@ import Icon from '@/components/Icon.vue'
 import { useStyleStore } from '@/stores/style.js'
 import MenuItem from '@/components/menu/Item.vue'
 import { useRouter } from 'vue-router'
+import { useMainStore } from '../../stores/main'
 
 const styleStore = useStyleStore()
 
@@ -126,5 +127,11 @@ const router = useRouter()
 router.beforeEach(() => {
     styleStore.isAsideMobileOpen = false
     styleStore.isNavMobileOpen = false
+})
+
+const mainStore = useMainStore()
+mainStore.setUser({
+    avatar: 'https://avatars.githubusercontent.com/u/2597771?v=4',
+    username: 'aaronzjc'
 })
 </script>
---
+++ src/stores/main.js
@@ -0,0 +1,20 @@
+import { defineStore } from 'pinia'
+
+export const useMainStore = defineStore('main', {
+    state: () => ({
+        userInfo: {
+            username: '',
+            avatar: ''
+        }
+    }),
+    actions: {
+        setUser(payload) {
+            if (payload.username) {
+                this.userInfo.username = payload.username
+            }
+            if (payload.avatar) {
+                this.userInfo.avatar = payload.avatar
+            }
+        }
+    }
+})
